ffd6402e7664cf18c7a38c8577c260411db1d977
MirkoDangelo06 <dangelo.mirko@itimeucci.com>
2024-10-21 16:24:21 +0200

    finale

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server Avviato");
+        ServerSocket ser1 = new ServerSocket(3000);
+
+        do{        
+        Socket socket = ser1.accept();
+        System.out.println("Client si è collegato");
+        
+        MioThread t = new MioThread(socket);
+        t.start();
+
+        }while(true);
+    }
+}
---
+++ src/main/java/com/example/MioThread.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MioThread extends Thread{
+    private Socket socket;
+        
+    
+    public MioThread(Socket socket) {
+            this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            String input1;
+            String inputOperazione;
+            boolean error = false;
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            do{
+                String ans = "Risposta ";
+                //leggo il primo input1
+                input1 = in.readLine();
+                System.out.println("String input1: " + input1);
+        
+                //leggo il secondo input e veridico l'input
+                    inputOperazione = in.readLine();
+                    System.out.println(inputOperazione);
+                    switch (inputOperazione) {
+                        case "M":
+                            ans += input1.toUpperCase();
+                        break;
+                        case "m":
+                            ans += input1.toLowerCase();
+                        break;
+                        case "r":
+                            ans += new StringBuilder(input1).reverse().toString();
+                        break;
+                        case "c":
+                            ans += Integer.toString(input1.length());
+                        break;
+                        case "ex":
+                            ans = "!";
+                        break;
+                        default:
+                            System.out.println("Valore errato");
+                            error = true;
+                        break;
+                    }
+                    if(ans.equals("!"))break;
+                if(!error){
+                    out.writeBytes(ans + "\n");
+                    System.out.println(ans);
+                }
+                else
+                {
+                    out.writeBytes("!!!\n");
+                }
+            }while(true);
+            
+            socket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
